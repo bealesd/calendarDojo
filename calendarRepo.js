c7efd41757dfb93cb93aa573a3dbc8bf56d82252
bealesd <david_beales@ymail.com>
2019-11-10 18:02:46 +0000

    stupid change

--- calendarRepo.js
+++ calendarRepo.js
@@ -1,7 +1,7 @@
 export class CalendarRepo {
     constructor() {
         this.calendarRepoUrl = 'http://localhost:1337/';
-        // this.calendarRepoUrl = 'https://calservice.azurewebsites.net/';
+        this.calendarRepoUrl = 'https://calservice.azurewebsites.net/';
     }
 
     getData(year, month) {
